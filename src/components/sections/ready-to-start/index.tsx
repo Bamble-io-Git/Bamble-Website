cd8c22cba3e551a22af17079353d508c723869b6
Benneth Uzochukwu <benneth@bamble.io>
2023-12-16 22:18:04 +0100

    building the card component

--- src/components/sections/ready-to-start/index.tsx
+++ src/components/sections/ready-to-start/index.tsx
@@ -1,11 +1,32 @@
 import { IMAGES } from "@/app/theme/images/images";
 import InfoComponent from "@/components/elements/info";
 import LinkComponent from "@/components/elements/link";
+import useMediaQuery from "@/hooks/useMediaQuery";
 import clsx from "clsx";
 import Image from "next/image";
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 
 const ReadyToGetStarted = forwardRef((props, scrollRef: any) => {
+  const isMobile = useMediaQuery(640);
+  const [limit, setLimit] = useState(3);
+  const [selectedCard, setSelectedCard] = useState(undefined);
+
+  const handleMore = (array: any[], limit: number) => {
+    let maxCheck = 0;
+
+    if (array.length > maxCheck) {
+      maxCheck = array.length;
+    }
+    if (array.length < maxCheck) {
+      console.log("Array", array);
+      const items = array.slice(0, limit);
+      return items;
+    } else {
+      const items = array.slice(0, limit + 1);
+      return items;
+    }
+  };
+
   const CARDS = [
     {
       title: "RAAS Basic",
@@ -49,8 +70,21 @@ const ReadyToGetStarted = forwardRef((props, scrollRef: any) => {
       ],
     },
   ];
+
+  const handleIncrementLoadMore = (index: number) => {
+    console.log(selectedCard);
+    console.log(index);
+    // setLimit((p) =>
+    //   index === selectedCard ? p * CARDS[0].incentives[0].items.length : 4,
+    // );
+
+    if (index === selectedCard) {
+      setLimit(200);
+    }
+  };
+
   return (
-    <section className=" wrapper py-20 sm:py-[110px]">
+    <section className="wrapper py-20 sm:py-[110px]">
       <div className="sm:text-start text-center mx-auto">
         <h2 className="text-black text-center">Ready to Get Started?</h2>
         <p className="text-[#434B53] text-lg sm:text-[20px] max-w-[420px] text-center mx-auto mt-5">
@@ -59,17 +93,17 @@ const ReadyToGetStarted = forwardRef((props, scrollRef: any) => {
         </p>
       </div>
 
-      <div className="flex justify-center mx-auto max-w-6xl mt-8 lg:mt-16">
+      <div className="flex flex-col sm:flex-row justify-center mx-auto max-w-6xl mt-8 lg:mt-16">
         <div
-          className="w-full flex justify-center gap-10 sm:gap-4"
+          className="w-full flex sm:justify-center gap-10 sm:gap-4 flex-col lg:flex-row items-center lg:items-start"
           ref={scrollRef}
         >
           {CARDS.map(({ title, pricing, incentives }, index) => (
             <div
-              className="max-w-[580px] flex flex-col gap-y-7 border bg-gray-primary h-fit"
+              className="max-w-[480px] xl:max-w-[580px] flex flex-col gap-y-7 border bg-gray-primary h-fit"
               key={title}
             >
-              <div className="relative w-[580px] h-[183px]">
+              <div className="relative hidden sm:block w-[480px] xl:w-[580px] h-[183px]">
                 <Image
                   src={IMAGES.colleague}
                   alt=""
@@ -104,14 +138,14 @@ const ReadyToGetStarted = forwardRef((props, scrollRef: any) => {
               </div>
 
               <div className="flex flex-col gap-y-7 px-10 pb-12">
-                {incentives.map((incentive) => (
+                {incentives.map((incentive, index) => (
                   <>
                     <p className="font-semibold text-[28px]">
                       {incentive.heading}
                     </p>
 
                     <div className="flex flex-col gap-y-5">
-                      {incentive.items.map((item) => (
+                      {handleMore(incentive.items, limit).map((item) => (
                         <span className="flex gap-x-3 items-center" key={item}>
                           <Image
                             alt=""
@@ -124,7 +158,7 @@ const ReadyToGetStarted = forwardRef((props, scrollRef: any) => {
                             <p
                               key={item}
                               className={clsx(
-                                item.includes("Intelligence") && "pb-7",
+                                item.includes("Intelligence") && "pb-6",
                                 "relative",
                               )}
                             >
@@ -151,6 +185,26 @@ const ReadyToGetStarted = forwardRef((props, scrollRef: any) => {
                     </div>
                   </>
                 ))}
+                {!isMobile && (
+                  <span className="flex gap-x-3 items-center">
+                    <Image
+                      alt=""
+                      width={30}
+                      height={30}
+                      src="/assets/down.svg"
+                    />
+                    <button
+                      onClick={() => {
+                        setSelectedCard(index);
+
+                        handleIncrementLoadMore(index);
+                      }}
+                      className="mr-auto text-[#323130] text-base font-normal"
+                    >
+                      See more
+                    </button>
+                  </span>
+                )}
               </div>
             </div>
           ))}
