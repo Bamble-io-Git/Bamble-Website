a7e7939ec291ec871d05efd3b629fffc2334c676
Benneth Uzochukwu <benneth@bamble.io>
2023-12-17 09:12:15 +0100

    adding card see more logic

--- src/app/theme/images/images.ts
+++ src/app/theme/images/images.ts
@@ -3,9 +3,8 @@ export const IMAGES = {
     "https://res.cloudinary.com/dotcu0ow9/image/upload/v1702478842/funcionario-do-sexo-masculino-se-acostumando-com-seu-novo-trabalho-de-escritorio-junto-com-colegas-do-sexo-feminino_1_hwpqh0.png",
   designer:
     "https://res.cloudinary.com/dotcu0ow9/image/upload/v1702478842/Design_sem_nome_1_nut2yp.png",
-  colleagues:
-    "https://res.cloudinary.com/dotcu0ow9/image/upload/v1702478842/colegas-fazendo-trabalho-em-equipe-para-um-projeto_2_gpz0c4.png",
-  colleague: "/assets/colleague.png",
+  colleague:
+    "https://res.cloudinary.com/dotcu0ow9/image/upload/v1702765458/colleague_wblvlp.png",
   jovial:
-    "https://res.cloudinary.com/dotcu0ow9/image/upload/v1702478840/jovens-empresarios-alegres-conversam-durante-o-intervalo-para-o-cafe-no-escritorio_1_rfjoot.png",
+    "https://res.cloudinary.com/dotcu0ow9/image/upload/v1702765458/jovial_belwhu.png",
 };
--- src/components/sections/ready-to-start/index.tsx
+++ src/components/sections/ready-to-start/index.tsx
@@ -6,80 +6,63 @@ import clsx from "clsx";
 import Image from "next/image";
 import { forwardRef, useState } from "react";
 
+const CARDS = [
+  {
+    id: 0,
+    title: "RAAS Basic",
+    pricing: "€ 1,000",
+    image: IMAGES.colleague,
+    incentives: [
+      {
+        heading: "What do you get?",
+        items: [
+          "One opening = one time fee",
+          "Profile definition based on company needs",
+          "Job description creation",
+          "Salary Intelligence per role and market",
+          "2-3 days to receive a shortlist",
+          "3 months candidate replacement guarantee",
+          "Support during the interviews",
+          "Slack Updates",
+        ],
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Subscription RAAS Pro",
+    pricing: "€ 2,500",
+    image: IMAGES.jovial,
+    incentives: [
+      {
+        heading: "What do you get?",
+        items: [
+          "Two role requests at a time = Uncapped requests",
+          "Unlimited recruiting requests",
+          "Embedded hiring",
+          "Salary Intelligence per role and market",
+          "Ready to hire shortlist",
+          "3 months candidate replacement guarantee",
+          "Priority Support",
+          "Slack Updates",
+          "+ other benefits offered in basic tier",
+        ],
+      },
+    ],
+  },
+];
+
 const ReadyToGetStarted = forwardRef((props, scrollRef: any) => {
   const isMobile = useMediaQuery(640);
-  const [limit, setLimit] = useState(3);
-  const [selectedCard, setSelectedCard] = useState(undefined);
+  const [limit, setLimit] = useState(2);
+  const [selectedCardIndex, setSelectedCardIndex] = useState<number>();
 
-  const handleMore = (array: any[], limit: number) => {
-    let maxCheck = 0;
-
-    if (array.length > maxCheck) {
-      maxCheck = array.length;
-    }
-    if (array.length < maxCheck) {
-      console.log("Array", array);
-      const items = array.slice(0, limit);
-      return items;
+  const handleMore = (array: any[], limit: number, selected: boolean) => {
+    if (!isMobile) return array;
+    if (selected && isMobile) {
+      return array.slice(0, limit + 10);
     } else {
-      const items = array.slice(0, limit + 1);
-      return items;
-    }
-  };
-
-  const CARDS = [
-    {
-      title: "RAAS Basic",
-      pricing: "€ 1,000",
-      image: IMAGES.colleague,
-      incentives: [
-        {
-          heading: "What do you get?",
-          items: [
-            "One opening = one time fee",
-            "Profile definition based on company needs",
-            "Job description creation",
-            "Salary Intelligence per role and market",
-            "2-3 days to receive a shortlist",
-            "3 months candidate replacement guarantee",
-            "Support during the interviews",
-            "Slack Updates",
-          ],
-        },
-      ],
-    },
-    {
-      title: "Subscription RAAS Pro",
-      pricing: "€ 2,500",
-      image: IMAGES.jovial,
-      incentives: [
-        {
-          heading: "What do you get?",
-          items: [
-            "Two role requests at a time = Uncapped requests",
-            "Unlimited recruiting requests",
-            "Embedded hiring",
-            "Salary Intelligence per role and market",
-            "Ready to hire shortlist",
-            "3 months candidate replacement guarantee",
-            "Priority Support",
-            "Slack Updates",
-            "+ other benefits offered in basic tier",
-          ],
-        },
-      ],
-    },
-  ];
-
-  const handleIncrementLoadMore = (index: number) => {
-    console.log(selectedCard);
-    console.log(index);
-    // setLimit((p) =>
-    //   index === selectedCard ? p * CARDS[0].incentives[0].items.length : 4,
-    // );
-
-    if (index === selectedCard) {
-      setLimit(200);
+      return array.slice(0, 4);
     }
   };
 
@@ -98,123 +81,145 @@ const ReadyToGetStarted = forwardRef((props, scrollRef: any) => {
           className="w-full flex sm:justify-center gap-14 lg:gap-4 flex-col lg:flex-row items-center lg:items-start"
           ref={scrollRef}
         >
-          {CARDS.map(({ title, pricing, incentives }, index) => (
-            <div
-              className="max-w-[480px] xl:max-w-[580px] flex flex-col gap-y-7 border bg-gray-primary h-fit sm:border-gray-200 border-primary"
-              key={title}
-            >
-              <div className="relative hidden sm:block w-[480px] xl:w-[580px] h-[183px]">
-                <Image
-                  src={IMAGES.colleague}
-                  alt=""
-                  fill
-                  className="object-cover w-full h-full"
-                  quality={100}
-                />
-              </div>
+          {CARDS.map(({ id, title, pricing, incentives, image }, index) => {
+            const isSelected = index === selectedCardIndex;
 
-              <div className="px-7 lg:px-10 flex flex-col gap-y-7 pb-10 pt-8 sm:pt-0">
-                {isMobile && title.includes("Pro") && (
-                  <div className="max-w-[128px]">
-                    <InfoComponent text="Recommended" />
-                  </div>
+            return (
+              <div
+                className={clsx(
+                  isMobile && isSelected
+                    ? "border border-blue-primary"
+                    : "border border-primary sm:border-[#D4D4D4]",
+
+                  "max-w-[480px] xl:max-w-[580px] flex flex-col gap-y-7 border bg-gray-primary h-fit relative transition-all",
                 )}
-                <span className="flex gap-4 items-center">
-                  <h4 className="font-semibold sm:text-[28px] text-2xl">
-                    {title}{" "}
-                  </h4>
-
-                  {!isMobile && title.includes("Pro") && (
-                    <InfoComponent text="Recommended" />
-                  )}
-                </span>
-
-                <span className="flex gap-x-3">
+                key={title}
+              >
+                <div className="relative hidden sm:block w-[480px] xl:w-[580px] h-[183px]">
                   <Image
-                    src="/assets/Money.svg"
+                    src={image}
                     alt=""
-                    width={30}
-                    height={30}
-                  />{" "}
-                  <p className="text-blue-primary"> Pricing: {pricing} </p>
-                </span>
-
-                <div className="w-2/3 mt-5 -mb-2">
-                  <LinkComponent text="Pay now" outline={index === 0} url="#" />
+                    fill
+                    className="object-cover w-full h-full"
+                    quality={100}
+                  />
                 </div>
-              </div>
 
-              <div className="flex flex-col gap-y-7 px-10 pb-12">
-                {incentives.map((incentive, index) => (
-                  <>
-                    <p className="font-semibold text-[28px]">
-                      {incentive.heading}
-                    </p>
-
-                    <div className="flex flex-col gap-y-5">
-                      {handleMore(incentive.items, limit).map((item) => (
-                        <span className="flex gap-x-3 items-center" key={item}>
-                          <Image
-                            alt=""
-                            width={30}
-                            height={30}
-                            src="/assets/blue-check.svg"
-                          />
-
-                          <span className="flex gap-x-3 items-center">
-                            <p
-                              key={item}
-                              className={clsx(
-                                item.includes("Intelligence") && "pb-6",
-                                "relative",
-                              )}
-                            >
-                              {item}
-
-                              {item.includes("Intelligence") && (
-                                <span className="text-xs sm:text-sm text-[#605E5C] font-normal absolute top-16 sm:top-7 left-0">
-                                  * only markets where we have presence
-                                </span>
-                              )}
-                            </p>
-
-                            {item.includes("Embedded hiring") && (
-                              <Image
-                                alt=""
-                                width={30}
-                                height={30}
-                                src="/assets/exclaim.svg"
-                              />
-                            )}
-                          </span>
-                        </span>
-                      ))}
+                <div className="px-7 lg:px-10 flex flex-col gap-y-7 pb-10 pt-8 sm:pt-0">
+                  {isMobile && title.includes("Pro") && (
+                    <div className="max-w-[128px]">
+                      <InfoComponent text="Recommended" />
                     </div>
-                  </>
-                ))}
-                {!isMobile && (
-                  <span className="flex gap-x-3 items-center">
+                  )}
+                  <span className="flex gap-4 items-center">
+                    <h4 className="font-semibold sm:text-[28px] text-2xl">
+                      {title}{" "}
+                    </h4>
+
+                    {!isMobile && title.includes("Pro") && (
+                      <InfoComponent text="Recommended" />
+                    )}
+                  </span>
+
+                  <span className="flex gap-x-3">
                     <Image
+                      src="/assets/Money.svg"
                       alt=""
                       width={30}
                       height={30}
-                      src="/assets/down.svg"
-                    />
-                    <button
-                      onClick={() => {
-                        setSelectedCard(index);
-
-                        handleIncrementLoadMore(index);
-                      }}
-                      className="mr-auto text-[#323130] text-base font-normal"
-                    >
-                      See more
-                    </button>
+                    />{" "}
+                    <p className="text-blue-primary"> Pricing: {pricing} </p>
                   </span>
-                )}
+
+                  <div className="w-2/3 mt-5 -mb-2">
+                    <LinkComponent
+                      text="Pay now"
+                      outline={index === 0}
+                      url="#"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex flex-col gap-y-7 px-10 pb-12">
+                  {incentives.map((incentive, idx) => {
+                    return (
+                      <>
+                        <p className="font-semibold text-[28px]">
+                          {incentive.heading}
+                        </p>
+
+                        <div className="flex flex-col gap-y-5">
+                          {handleMore(
+                            incentive.items,
+                            index === 0 ? limit : limit + 1,
+                            isSelected,
+                          )?.map((item) => {
+                            return (
+                              <span
+                                className="flex gap-x-3 items-center"
+                                key={item}
+                              >
+                                <Image
+                                  alt=""
+                                  width={30}
+                                  height={30}
+                                  src="/assets/blue-check.svg"
+                                />
+
+                                <span
+                                  className={clsx(
+                                    item.includes("Intelligence") &&
+                                      "relative pb-6",
+                                    "flex gap-x-3 items-center",
+                                  )}
+                                >
+                                  {item.includes("Intelligence") && (
+                                    <span className="text-xs sm:text-sm text-[#605E5C] font-normal absolute top-16 sm:top-7 left-0">
+                                      * only markets where we have presence
+                                    </span>
+                                  )}
+                                  <p key={item}>{item}</p>
+
+                                  {item.includes("Embedded hiring") && (
+                                    <Image
+                                      alt=""
+                                      width={30}
+                                      height={30}
+                                      src="/assets/exclaim.svg"
+                                    />
+                                  )}
+                                </span>
+                              </span>
+                            );
+                          })}
+                        </div>
+                      </>
+                    );
+                  })}
+
+                  {isMobile && (
+                    <span className="flex gap-x-3 items-center">
+                      <Image
+                        alt=""
+                        width={30}
+                        height={30}
+                        src="/assets/down.svg"
+                      />
+                      <button
+                        onClick={() => {
+                          setSelectedCardIndex(id);
+                          setLimit((p) => p + incentives[0].items.length);
+                        }}
+                        className="mr-auto text-[#323130] text-base font-normal"
+                      >
+                        See more
+                      </button>
+                    </span>
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </section>
