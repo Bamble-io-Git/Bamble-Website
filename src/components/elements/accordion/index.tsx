e855f2283b508a2178826edd68a61e6ecc9cd4e6
Benneth Uzochukwu <benneth@bamble.io>
2023-12-18 15:59:19 +0100

    building the accordion component

--- src/components/elements/accordion/index.tsx
+++ src/components/elements/accordion/index.tsx
@@ -1,98 +1,61 @@
-// import { useState } from "react";
-// type TAccordion = {
-//   title: string;
-//   content: string;
-//   icon: "transparent" | "profitability" | "engagement" | "limitless";
-// };
+import clsx from "clsx";
+import Image from "next/image";
+import { useState } from "react";
+type TAccordion = {
+  title: string;
+  content: string;
+  icon: "transparent" | "profitability" | "engagement" | "limitless";
+};
 
-// const Accordion = ({ title, content, icon }: TAccordion) => {
-//   const [isOpen, setIsOpen] = useState(false);
+const Accordion = ({ title, content, icon }: TAccordion) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-//   const handleToggle = () => {
-//     setIsOpen(!isOpen);
-//   };
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-//   const toggleAccordion = () => {
-//     setIsOpen(!isOpen);
-//   };
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
 
-//   return (
-//     // <div className={styles.wrapper}>
-//     //   {isOpen && <p className={styles.content}>{content}</p>}
-//     //   <button className={styles.button} onClick={handleToggle}>
-//     //     <p>icon</p>
-
-//     //     <p className={styles.title}>{title}</p>
-//     //     <div className={styles.chevron}>
-//     //       <span>{isOpen ? <p>up</p> : <p>down</p>}</span>
-//     //     </div>
-//     //   </button>
-//     // </div>
-
-//     <div className="relative w-full max-w-md mx-auto mb-4">
-//       <div
-//         className={`absolute w-full bg-gray-100 p-4 rounded-md transition-transform duration-300 ${
-//           isOpen ? "transform translate-y-12" : "transform -translate-y-full"
-//         }`}
-//       >
-//         {content}
-//       </div>
-//       <button
-//         className="w-full bg-blue-500 text-white py-2 px-4 mb-2 rounded-md transition-transform duration-300 transform hover:scale-105 focus:outline-none relative z-10"
-//         onClick={toggleAccordion}
-//       >
-//         {title}
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Accordion;
-
-// Accordion.js
-
-const Accordion = ({ question, onClick }) => {
   return (
-    <div className="relative w-full max-w-md mx-auto mb-4">
-      <button
-        className="w-full bg-blue-500 text-white py-2 px-4 mb-2 rounded-md transition-transform duration-300 transform hover:scale-105 focus:outline-none relative z-10"
-        onClick={onClick}
+    <div className="relative transition-all ease-in-out">
+      <div
+        className={`absolute bg-purple-primary rounded-2xl text-white-primary   transition-transform duration-300 w-[680px] p-9 ${
+          isOpen
+            ? "transform translate-y-12 mt-[100px]"
+            : "transform -translate-y-full"
+        }`}
       >
-        {question}
+        {content}
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+        numquam harum saepe in consectetur minus, suscipit quasi placeat
+        consequatur architecto provident similique voluptas, illum id!
+        Recusandae suscipit repellast laborum ipsa!
+      </div>
+      <button
+        className={clsx(
+          isOpen ? "mt-[0px]" : "mt-[20px] border",
+          "bg-white-primary p-9 rounded-md transition-transform duration-300 transform focus:outline-none relative z-10 w-[680px] text-gray-text flex justify-between items-center",
+        )}
+        onClick={toggleAccordion}
+      >
+        {title}
+        <p className="text-[20px] font-normal">
+          {" "}
+          consequatur architecto provident similique voluptas, illum id!
+          Recusandae suscipit repellast laborum ipsa!{" "}
+        </p>
+
+        <Image
+          src="/assets/socials/ArrowDown.svg"
+          width={24}
+          height={24}
+          alt=""
+        />
       </button>
     </div>
   );
 };
 
-// AccordionList.js
-import { useState } from "react";
-
-const AccordionList = () => {
-  const [questions, setQuestions] = useState([
-    "Question 1",
-    "Question 2",
-    "Question 3",
-    // Add more questions as needed
-  ]);
-
-  const handleQuestionClick = (index) => {
-    const updatedQuestions = [...questions];
-    const selectedQuestion = updatedQuestions.splice(index, 1)[0];
-    updatedQuestions.unshift(selectedQuestion);
-    setQuestions(updatedQuestions);
-  };
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
-      {questions.map((question, index) => (
-        <Accordion
-          key={index}
-          question={question}
-          onClick={() => handleQuestionClick(index)}
-        />
-      ))}
-    </div>
-  );
-};
-
-export default AccordionList;
+export default Accordion;
--- src/components/sections/faq/index.tsx
+++ src/components/sections/faq/index.tsx
@@ -1,3 +1,4 @@
+import AccordionList from "@/components/elements/accordion";
 import Image from "next/image";
 
 const FAQ = () => {
@@ -16,7 +17,7 @@ const FAQ = () => {
     },
   ];
   return (
-    <section className="bg-blue-primary wrapper py-20 sm:py-[90px] relative flex justify-between">
+    <section className="bg-blue-primary wrapper py-10 sm:py-20 relative flex  justify-between">
       <Image
         src="/assets/inverted-z.svg"
         alt=""
@@ -24,19 +25,14 @@ const FAQ = () => {
         height={584}
         className="absolute -left-5 2xl:left-0 -bottom-0 2xl:-bottom-0 md:block hidden"
       />
-      <div className="relative sm:text-start text-center border max-w-[350px]">
+      <div className="relative sm:text-start text-center  w-[350px]">
         <h2 className="text-white-primary sm:block hidden">
           FAQ´s Frequently asked questions
         </h2>
       </div>
 
-      <div className="flex justify-between max-w-6xl mt-12 lg:mt-40 border">
-        <div className="w-full lg:w-[392px] flex flex-col gap-y-12"></div>
-        <div className=" rounded-t-[36px] relative h-[517px] w-[502px] lg:block hidden">
-          {/* <Accordion title="A" content="amaben" icon="sas" /> */}
-
-          {/* <AccordionList /> */}
-        </div>
+      <div className="flex   max-w-6xl mt-12 lg:mt-40 ">
+        <AccordionList />
       </div>
     </section>
   );
