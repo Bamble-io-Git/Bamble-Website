86dc19d1907adb7d182ca714e44ef2e43313508b
Benneth Uzochukwu <benneth@bamble.io>
2023-12-18 21:16:01 +0100

    making logo a link

--- src/components/sections/header/index.tsx
+++ src/components/sections/header/index.tsx
@@ -2,13 +2,14 @@
 import LinkComponent from "@/components/elements/link";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   const isMobile = useMediaQuery(640);
 
   return (
     <header className="wrapper py-7 bg-none sm:bg-gray-primary flex justify-between items-center sm:sticky top-0 w-full z-20 ">
-      <div className="flex gap-x-3 items-center mx-auto sm:mx-0">
+      <Link href="/" className="flex gap-x-3 items-center mx-auto sm:mx-0">
         <Image
           alt="Logo"
           src="/assets/bamble-logo.svg"
@@ -21,7 +22,7 @@ const Header = () => {
           width={isMobile ? 102 : 156}
           height={isMobile ? 15 : 24}
         />
-      </div>
+      </Link>
       <div className="sm:block hidden">
         <LinkComponent
           url="https://calendly.com/bamble-melissa/30min"
