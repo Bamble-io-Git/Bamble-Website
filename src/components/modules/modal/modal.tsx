273cce6b3cab58966414d1d3c81ae831069c8f61
Benneth Uzochukwu <benneth@bamble.io>
2023-12-19 16:17:07 +0100

    adding modal and improving faq

--- src/components/modules/modal/modal.tsx
+++ src/components/modules/modal/modal.tsx
@@ -5,33 +5,32 @@ const Modal = ({
   handleClose,
 }: {
   isOpen: boolean;
-  handleClose: () => void;
+  handleClose?: () => void;
 }) => {
   if (!isOpen) return null;
   return createPortal(
     <>
-      <div
-        className="top-0 fixed z-50 bottom-0 right-0 left-0 bg-[#00000057]"
-        onClick={handleClose}
-      />
-      <div className="fixed top-[30%] left-[7%] md:left-[16%] z-50">
-        <div className="relative rounded-2xl p-20 max-w-[95%] md:max-w-[80%] bg-[#fefefe] ">
-          <div className="flex gap-4">
+      <div className="top-0 fixed z-50 bottom-0 right-0 left-0 bg-[#00000057]" />
+      <div className="fixed top-[30%] left-[7%] md:left-[28%] z-50">
+        <div className="relative rounded-[26px] p-10 max-w-[95%] md:max-w-[60%] bg-white-primary">
+          <div className="flex gap-4 items-start">
             <div>
-              <Image alt="" width={230} height={30} src="/assets/exclaim.svg" />
+              <Image alt="" width={90} height={30} src="/assets/exclaim.svg" />
             </div>
             <div>
+              <p className="font-semibold mb-6"> Embbeded hiring</p>
               <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae,
-                obcaecati ipsam. Iste architecto voluptatem odit sunt quia
-                perspiciatis consequatur? Cupiditate eveniet quos ipsam neque,
-                quod numquam. Illo perferendis praesentium veritatis.
+                We integrate our process with yours, streamlining post-sourcing
+                processes such as technical testing, profile review etc.) and
+                turbocharging your speed. Candidates are checked and vetted
+                upfront.
               </p>
             </div>
           </div>
         </div>
       </div>
     </>,
+    //@ts-ignore
     document.getElementById("portal"),
   );
 };
