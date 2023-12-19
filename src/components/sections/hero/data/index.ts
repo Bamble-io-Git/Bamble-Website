332bcef74c790e163d9ea4e145f597ffa7ef7ce0
Benneth Uzochukwu <benneth@bamble.io>
2023-12-19 14:11:40 +0100

    fixing hero bug

---
+++ src/components/sections/hero/data/index.ts
@@ -0,0 +1,17 @@
+export const performanceIndex = [
+  {
+    id: 0,
+    metric: "96%",
+    outcome: "Time saved",
+  },
+  {
+    id: 1,
+    metric: "5X",
+    outcome: "more qualified referrals",
+  },
+  {
+    id: 2,
+    metric: "+10",
+    outcome: "countries reached",
+  },
+];
--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -5,33 +5,16 @@ import LinkComponent from "@/components/elements/link";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
+import { performanceIndex } from "./data";
 
 const Hero = ({ handleScroll }: { handleScroll: () => void }) => {
   const [selectedCard, setSelectedCard] = useState(0);
-
-  const performanceIndex = [
-    {
-      id: 0,
-      metric: "96%",
-      outcome: "Time saved",
-    },
-    {
-      id: 1,
-      metric: "5X",
-      outcome: "more qualified referrals",
-    },
-    {
-      id: 2,
-      metric: "+10",
-      outcome: "countries reached",
-    },
-  ];
 
   const handleSelectCard = useCallback(() => {
     setSelectedCard((previousState) =>
       previousState === performanceIndex.length - 1 ? 0 : previousState + 1,
     );
-  }, [performanceIndex.length]);
+  }, []);
 
   useEffect(() => {
     const intervalId = setInterval(handleSelectCard, 1200);
@@ -56,24 +39,24 @@ const Hero = ({ handleScroll }: { handleScroll: () => void }) => {
         width={isTablet ? 220 : 477}
         height={400}
       />
-      <div className="flex justify-center flex-col text-center gap-y-6 pt-5 sm:pt-12 pb-20 max-w-[728px] mx-auto px-7 md:px-0">
-        <h1>
+      <div className="flex justify-center flex-col text-center gap-y-6 pt-5 sm:pt-12 pb-20 max-w-[728px] mx-auto px-7 md:px-0 z-10">
+        <h1 className="z-10">
           Affordable{" "}
           <span className="text-blue-primary">Recruitment as a Service</span>{" "}
           for Start-ups & Companies
         </h1>
 
-        <h2 className="text-gray-text text-lg sm:text-[20px] font-primary leading-[155%] sm:leading-7 font-normal">
+        <h2 className="text-gray-text text-lg sm:text-[20px] font-primary leading-[155%] sm:leading-7 font-normal z-10">
           Do you struggle to find top tier tech talent within your budget?
         </h2>
-        <h2 className="text-gray-text text-lg sm:text-[20px] font-primary leading-[155%] sm:leading-7 font-normal">
+        <h2 className="text-gray-text text-lg sm:text-[20px] font-primary leading-[155%] sm:leading-7 font-normal z-10">
           {" "}
           We have been there too. We understand the pain, the wasted time and
           the frustration that comes with limited resources. Our aim is to fix
           these!
         </h2>
 
-        <div className="flex flex-col sm:flex-row justify-center gap-5 pt-6 pb-7 sm:mx-0 mx-auto">
+        <div className="flex flex-col sm:flex-row justify-center gap-5 pt-6 pb-7 sm:mx-0 mx-auto z-10">
           <LinkComponent
             url="https://calendly.com/bamble-melissa/30min"
             text="Book a call"
