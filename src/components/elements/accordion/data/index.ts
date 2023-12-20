025641962b52cd2daf1881d9db7aa6611af326d8
Benneth Uzochukwu <benneth@bamble.io>
2023-12-20 08:58:24 +0100

    updating accordion text

--- src/components/elements/accordion/data/index.ts
+++ src/components/elements/accordion/data/index.ts
@@ -7,27 +7,27 @@ export const AccordionData = [
   {
     title: "Can I cancel my subscription anytime?",
     content:
-      "Our subscription process is a monthly plan that grants you access to our in-house team of senior technical recruiters dedicated to helping you grow. Their expertise turbocharges your recruiting process, focusing on what matters! Simply submit your role, and our team will deliver high-quality candidates.",
+      "100%, at anytime. We want to provide flexibility to our clients, and we are confident that once you see the results, you will stay with us!",
   },
   {
     title:
       "Can I provide feedback and request revisions during the recruitment process?",
     content:
-      "Our subscription process is a monthly plan that grants you access to our in-house team of senior technical recruiters dedicated to helping you grow. Their expertise turbocharges your recruiting process, focusing on what matters! Simply submit your role, and our team will deliver high-quality candidates.",
+      "We have designed a process where your input is key to achieve the expected results. Our team will take your feedback into account to guarantee that the talents presented meet your results.",
   },
   {
     title: "What if I have specific tests that candidates should pass?",
     content:
-      "Our subscription process is a monthly plan that grants you access to our in-house team of senior technical recruiters dedicated to helping you grow. Their expertise turbocharges your recruiting process, focusing on what matters! Simply submit your role, and our team will deliver high-quality candidates.",
+      "Our goal is to make your process fluid and save time. We will align those requirements at the beginning to ensure that all candidates presented have already complied with the vetting.",
   },
   {
     title: "Can I upgrade or downgrade my subscription plan?",
     content:
-      "Our subscription process is a monthly plan that grants you access to our in-house team of senior technical recruiters dedicated to helping you grow. Their expertise turbocharges your recruiting process, focusing on what matters! Simply submit your role, and our team will deliver high-quality candidates.",
+      "Absolutely! We have designed the plans to meet your specific needs. You can upgrade or downgrade your plan at any point, you will just need to reach out to our team and we will assist you.",
   },
   {
     title: "Do you offer ongoing support after the role is filled?",
     content:
-      "Our subscription process is a monthly plan that grants you access to our in-house team of senior technical recruiters dedicated to helping you grow. Their expertise turbocharges your recruiting process, focusing on what matters! Simply submit your role, and our team will deliver high-quality candidates.",
+      "Yes, we do! We offer post-hire support and assistance services, long teem relationships are valuable to us and we are invested in your success as much you are.",
   },
 ];
