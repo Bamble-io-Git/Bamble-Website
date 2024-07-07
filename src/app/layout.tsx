dc6ed4e41b431eba250d2330c49793bf5f2bb0d5
Benneth Uzochukwu <benneth@bamble.io>
2024-07-07 03:16:08 -0700

    Seo implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,16 @@
-import Footer from "@/components/sections/footer";
-import Header from "@/components/sections/header";
-import type { Metadata } from "next";
+import Footer from '@/components/sections/footer';
+import Header from '@/components/sections/header';
+import type { Metadata } from 'next';
 
-import GTag from "@/scripts/google-tag";
-import "./globals.css";
-import { montserrat, notoSans } from "./theme/fonts";
+import GTag from '@/scripts/google-tag';
+import './globals.css';
+import { montserrat, notoSans } from './theme/fonts';
 export const metadata: Metadata = {
-  title: "Bamble",
-  description: "Affordable Recruitment as a Service for Start-ups & Companies",
+  title: 'Bamble App',
+  description: 'Create AI powered high-quality CV in 3 clicks',
+  applicationName: 'Bamble CV Creator',
+  generator: 'Next.js',
+  keywords: 'CV, Resume, AI, CV Builder, Resume Builder',
 };
 
 export default function RootLayout({
