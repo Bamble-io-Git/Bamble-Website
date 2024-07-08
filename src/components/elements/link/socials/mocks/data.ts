bfb5e77019bcb87bc5264a4ffe6af40c074164ae
Benneth Uzochukwu <benneth@bamble.io>
2024-07-08 14:07:44 -0700

    updating socials tiktok

--- src/components/elements/link/socials/mocks/data.ts
+++ src/components/elements/link/socials/mocks/data.ts
@@ -2,6 +2,7 @@ const icons = {
   twitter: '/assets/socials/Twitter.svg',
   facebook: '/assets/socials/Facebook.svg',
   linkedin: '/assets/socials/Linkedin.svg',
+  tiktok: '/assets/socials/Tiktok.svg',
   instagram: '/assets/socials/Instagram.svg',
 };
 
@@ -11,10 +12,14 @@ export const SOCIAL_LINKS = [
     logo: icons.facebook,
   },
   { url: '#', logo: icons.twitter },
-
+  {
+    url: 'https://www.tiktok.com/@bamble689?_t=8njwUR51EIb&_r=1',
+    logo: icons.tiktok,
+  },
   {
     url: 'https://instagram.com/bamble.io?igshid=YmMyMTA2M2Y=',
     logo: icons.instagram,
   },
+
   { url: 'https://www.linkedin.com/company/bamble', logo: icons.linkedin },
 ];
