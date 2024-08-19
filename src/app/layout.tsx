a8602ccc6f8efb57c2924800249c76fed27b4040
Benneth Uzochukwu <benneth@bamble.io>
2024-08-19 19:21:33 +0100

    gtag refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import Footer from '@/components/sections/footer';
-import Header from '@/components/sections/header';
+// import Footer from '@/components/sections/footer';
+// import Header from '@/components/sections/header';
 import type { Metadata } from 'next';
-
-import GTag from '@/scripts/google-tag';
+import { GoogleAnalytics } from '@next/third-parties/google';
+// import GTag from '@/scripts/google-tag';
 import './globals.css';
 import { montserrat, notoSans } from './theme/fonts';
 export const metadata: Metadata = {
@@ -20,13 +20,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <GTag />
       <body
         className={`${montserrat.variable} ${notoSans.variable}`}
         id="portal"
       >
+        {/* <GTag /> */}
+
         {children}
       </body>
+      <GoogleAnalytics gaId="G-TJESG59G90" />
     </html>
   );
 }
