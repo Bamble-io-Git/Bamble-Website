56c0909d90409f6e3eefe3c7b256b3903c407c56
Benneth Uzochukwu <benneth@bamble.io>
2024-09-20 10:09:36 +0100

    cv upload improvement

--- src/components/elements/pdf-uploader/index.tsx
+++ src/components/elements/pdf-uploader/index.tsx
@@ -78,7 +78,7 @@ function MyDropzone({ setFile, file }: { setFile: (file: File) => void }) {
     <div className="w-[592px]">
       <div
         {...getRootProps()}
-        className="border-dashed border-2 p-8 min-w-full flex justify-between max-h-[100px]"
+        className="border-dashed border-2 p-8 min-w-full flex  max-h-[100px]"
       >
         <input {...getInputProps()} />
 
@@ -123,7 +123,7 @@ function MyDropzone({ setFile, file }: { setFile: (file: File) => void }) {
           />
         </svg>
 
-        <div>
+        <div className="ml-5">
           <p className="text-[13px] font-semibold">
             {isDragActive
               ? 'You have selected a file'
