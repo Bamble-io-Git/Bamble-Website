e19b941e6b40890bf523559893324ff9109e697d
Benneth Uzochukwu <benneth@bamble.io>
2024-10-29 09:20:15 -0500

    linkedin url fix

--- src/app/signup/schema/final-data.ts
+++ src/app/signup/schema/final-data.ts
@@ -1,8 +1,8 @@
 import z from 'zod';
 
 export const finalDataValidation = z.object({
-  linkedin_link: z.string().url(),
-  job_description_link: z.string().nullable(),
+  linkedin_link: z.string(),
+  job_description_link: z.string(),
 });
 
 export type TJobFilterSchema = z.infer<typeof finalDataValidation>;
